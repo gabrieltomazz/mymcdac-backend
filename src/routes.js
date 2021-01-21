c72eba666eacccc09488284db19b1f8d15b5b6f8
Gabriel Tomaz <gabrieltomazlima@hotmail.com>
2021-01-20 22:07:37 -0300

    Session & Update User

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,18 @@ import { Router } from 'express';
 import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.get('/', async (req, res) =>  {
+routes.post('/sessions', SessionController.store);
 
-});
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 
 export default routes;
